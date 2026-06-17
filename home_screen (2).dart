e8b9e2fe656f701025be1956373076c362df0dbc
mahamedkk111 <muhammadlawalsanik@gmail.com>
2026-06-17 12:08:43 +0100

    Delete home_screen (2).dart

--- home_screen (2).dart
+++
@@ -1,9 +0,0 @@
-class Customer {
-  final int id;
-  final String name;
-
-  Customer({required this.id, required this.name});
-
-  factory Customer.fromMap(Map<String, dynamic> m) =>
-      Customer(id: m['id'] as int, name: m['name'] as String);
-}
